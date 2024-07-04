95e28278f916cd61e743c33cb2e6f127ea98f9ac
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2024-07-04 22:15:22 +0900

    Time: 46 ms (50.83%), Space: 18 MB (20.54%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,10 @@
+import re
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+        s = re.sub(r'[^a-zA-Z0-9]','',s)
+        if s == s[::-1]:
+            return True
+        else:
+            return False
