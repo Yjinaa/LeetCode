901947ec7155bc9b669120c2a7089daf9419a845
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2024-07-17 13:56:02 +0900

    Time: 30 ms (83.79%), Space: 16.5 MB (52.24%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,10 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 0 or n==1:
+            return 1
+        if n==2:
+            return 2
+        a,b = 1,2
+        for i in range(2, n):
+            a,b = b, a+b
+        return b
