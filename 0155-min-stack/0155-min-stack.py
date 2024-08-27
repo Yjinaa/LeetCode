2aa14528a97895a782e3fafd7059b1498862a619
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2024-08-28 00:25:51 +0900

    Time: 47 ms (90.92%), Space: 20.3 MB (87.04%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -1,10 +1,16 @@
+import sys
 class MinStack:
 
     def __init__(self):
         self.s = []
+        self.min_s = []
+        self.min_val = sys.maxsize
         
     def push(self, val: int) -> None:
         self.s.append(val)
+        if val <= self.min_val:
+            self.min_val = val
+            self.min_s.append(self.min_val)
 
     def pop(self) -> None:
         if len(self.s) == 0:
@@ -12,15 +18,20 @@ class MinStack:
         else:
             top = self.top()
             self.s = self.s[:-1]
+            if top == self.min_val:
+                self.min_s = self.min_s[:-1]
+                if len(self.min_s) != 0:
+                    self.min_val = self.min_s[-1]
+                else:
+                    self.min_val = sys.maxsize
             return top
-            
 
     def top(self) -> int:
         return self.s[-1]
         
 
     def getMin(self) -> int:
-        return min(self.s)
+        return self.min_s[-1]
         
 
 
