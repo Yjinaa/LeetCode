eb5b56fb82205943103d136acb45d08516543b68
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2024-09-02 23:06:23 +0900

    Time: 232 ms (79.51%), Space: 18.8 MB (82.6%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -2,27 +2,24 @@ from collections import deque
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         self.m,self.n = len(grid), len(grid[0])
-        self.dxs = [0,1,0,-1]
-        self.dys = [1,0,-1,0]
-        visited = [[False]*self.n for _ in range(self.m)]
-        queue = deque()
+        directions = [(0,1),(1,0),(-1,0),(0,-1)]
         islands = 0
         for i in range(self.m):
             for j in range(self.n):
-                if visited[i][j] == False and grid[i][j]=="1":
-                    self.bfs((i,j), visited, grid, queue)
+                if grid[i][j]=="1":
+                    self.bfs((i,j), grid, directions)
                     islands += 1
         return islands
 
-    def bfs(self, start, visited, grid, queue):
+    def bfs(self, start, grid, directions):
+        queue = deque()
         queue.append(start)
+        grid[start[0]][start[1]] = "0"
         while queue:
             x,y = queue.popleft()
-            visited[x][y] = True
-            for dx, dy in zip(self.dxs, self.dys):
+            for dx, dy in directions:
                 nx, ny = x + dx, y + dy
-                if 0<=nx<self.m and 0<=ny<self.n and not visited[nx][ny] and grid[nx][ny] == "1":
+                if 0<=nx<self.m and 0<=ny<self.n and grid[nx][ny] == "1":
+                    grid[nx][ny] = "0"
                     queue.append((nx,ny))
-                    visited[nx][ny] = True
-
             
